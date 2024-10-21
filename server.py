580face58790653e9b0d99c2f957e4c29e2706b3
Leonty <krokxa228@gmail.com>
2024-10-21 17:18:42 +0500

    bugfix

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
+import time
+
 from flask import Flask, request, jsonify
 from Yandex_map_parser import YandexMapParser
 import functions
-import time
 
 app = Flask(__name__)
 parser = YandexMapParser()
+
 
 @app.route('/get_location', methods=['GET'])
 def get_location():
@@ -21,14 +23,14 @@ def get_location():
         return jsonify(location_from_NOMI)
     else:
         location = parser.get_location_from_Yandex(functions.modify_address_for_Yandex(address))
-
+        print(location)
         if location is None:
             parser.clear_cookies()
             parser.restart_browser()
             return jsonify({"error": "Location not found"}), 404
         else:
             if not location['Input_not_found']:
-                if functions.check_address_correct(address, house_number):
+                if functions.check_address_correct(location['Yandex_address'], house_number):
                     return jsonify([float(location['latitude']), float(location['longitude'])])
                 else:
                     return jsonify({"error": "Location not found"}), 404
@@ -36,8 +38,7 @@ def get_location():
         time.sleep(5)  # Подождите перед повторной попыткой
         return jsonify({"error": "Location not found. Maybe script got captcha!"}), 404
 
+
+
 if __name__ == '__main__':
-    try:
-        app.run(host='0.0.0.0', port=8080)
-    finally:
-        parser.stop_browser()  # Убедитесь, что Xvfb остановлен при выходе
+    app.run(host='0.0.0.0', port=8080)
